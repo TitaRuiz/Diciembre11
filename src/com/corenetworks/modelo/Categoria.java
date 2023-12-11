e91b2c0aa951b6938a149549edab28784e36c043
TitaRuiz <ggp2002es@gmail.com>
2023-12-11 19:55:00 +0100

    final clase

--- src/com/corenetworks/modelo/Categoria.java
+++ src/com/corenetworks/modelo/Categoria.java
@@ -1,2 +1,65 @@
-package com.corenetworks.modelo;public class Categoria {
+package com.corenetworks.modelo;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private int id_categoria;
+    private String nombre;
+    private String descripcion;
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id_categoria=" + id_categoria +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id_categoria == categoria.id_categoria && Objects.equals(nombre, categoria.nombre) && Objects.equals(descripcion, categoria.descripcion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_categoria, nombre, descripcion);
+    }
+
+    public Categoria() {
+    }
+
+    public Categoria(int id_categoria, String nombre, String descripcion) {
+        this.id_categoria = id_categoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
